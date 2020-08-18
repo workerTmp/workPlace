171c96bca4f8730e856de13119e7d11d09bb03d1
ihsinme <61293369+ihsinme@users.noreply.github.com>
2020-08-18 10:06:13 +0300

    Update teln.py

--- in/pdnsSql/teln.py
+++ in/pdnsSql/teln.py
@@ -1,1 +1,10 @@
+import telnetlib
 
+host = "127.0.0.1"
+port = 5300
+timeout = 10
+
+with telnetlib.Telnet(host, port, timeout) as session:
+    session.write(b"00")
+    for i in range(0x3030):
+        session.write(b"p")
