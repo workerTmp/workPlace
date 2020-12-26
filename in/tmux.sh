e7ba757d7198eb9b4ad4b09c947ed3cab2c8cf30
ihsinme <61293369+ihsinme@users.noreply.github.com>
2020-12-26 17:52:39 +0300

    Update tmux.sh

--- in/tmux.sh
+++ in/tmux.sh
@@ -5,6 +5,4 @@ tmux new-session -d
 tmux split-window -v
 tmux split-window -v 'sudo iftop'
 tmux split-window -h 'export NMON=c&&nmon'
-[ -f "/opt/RM.sh" ] || tmux split-window -h 'sh /opt/RM.sh' 
-#tmux new-window 'sudo top'
-tmux -2 attach-session -d
+tmux split-window -h 'sudo /bin/sh /opt/RM.sh' 
