bf4a3410a4d12e155b5bfc4e10746b1bf6d79bb4
ihsinme <61293369+ihsinme@users.noreply.github.com>
2020-12-26 17:34:16 +0300

    Update tmux.sh

--- in/tmux.sh
+++ in/tmux.sh
@@ -3,7 +3,7 @@ sudo apt install iftop
 sudo apt install nmon
 tmux new-session -d
 tmux split-window -v
-tmux split-window -v 'sudo iftop''export NMON=c&&nmon'
+tmux split-window -v 'sudo iftop'
 tmux split-window -h 'export NMON=c&&nmon'
 [ -f "/opt/RM.sh" ] || tmux split-window -h 'sh /opt/RM.sh' 
 #tmux new-window 'sudo top'
