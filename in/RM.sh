fc4cf1609d747852e3b3dae4d794cbe25e6eb034
ihsinme <61293369+ihsinme@users.noreply.github.com>
2020-12-27 14:13:05 +0300

    Update RM.sh

--- in/RM.sh
+++ in/RM.sh
@@ -3,3 +3,6 @@
 echo "curl -F \"file=@test.txt\" https://file.io"
 echo "7z x /tmp/1.7z -o\"/tmp/in\" -p123321"
 echo "7z a -ssw -mx1 -p123321 -r0 /tmp/2.7z /tmp/out/"
+while true; do
+sleep 3000
+done
