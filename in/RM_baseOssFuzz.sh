bd636b87075b6aff3ea09f3b863ed786b11abf97
ihsinme <61293369+ihsinme@users.noreply.github.com>
2020-12-27 15:18:45 +0300

    Create RM_baseOssFuzz.sh

---
+++ in/RM_baseOssFuzz.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "sh works/create.sh medovecot -only build fuzzers"
+echo "sh works/create.sh medovecot fuzz_test-mbox-from build fuzzers and start targets"
+echo "sh works/create.sh medovecot fuzz_test-mbox-from 1 only start targets"
+echo "curl -F \"file=@test.txt\" https://file.io"
+echo "7z x /tmp/1.7z -o\"/tmp/in\" -p123321"
+echo "7z a -ssw -mx1 -p123321 -r0 /tmp/2.7z /tmp/out/"
+while true; do
+sleep 3000
+done
