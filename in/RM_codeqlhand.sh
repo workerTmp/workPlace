e05b257f433e2d01f1d40c94d5d5d3d73d94acf8
ihsinme <61293369+ihsinme@users.noreply.github.com>
2020-12-27 15:16:34 +0300

    Update RM_codeqlhand.sh

--- in/RM_codeqlhand.sh
+++ in/RM_codeqlhand.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 echo "export PATH=/opt/codeqlmy/codeql:$PATH"
 echo "codeql resolve languages"
@@ -8,6 +8,9 @@ echo "codeql generate query-help --format=markdown"
 echo "codeql database create cpp-database --language=cpp --command=make"
 echo "codeql query run file.ql -d=db1"
 echo "codeql test run file.qlref"
+echo "curl -F \"file=@test.txt\" https://file.io"
+echo "7z x /tmp/1.7z -o\"/tmp/in\" -p123321"
+echo "7z a -ssw -mx1 -p123321 -r0 /tmp/2.7z /tmp/out/"
 while true; do
 sleep 3000
 done
