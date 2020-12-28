5259accc4020a75ce7a497d4248d9df90c76449c
ihsinme <61293369+ihsinme@users.noreply.github.com>
2020-12-28 18:25:12 +0300

    Update RM_baseOssFuzz.sh

--- in/RM_baseOssFuzz.sh
+++ in/RM_baseOssFuzz.sh
@@ -3,6 +3,8 @@
 echo "sh works/create.sh medovecot -only build fuzzers"
 echo "sh works/create.sh medovecot fuzz_test-mbox-from build fuzzers and start targets"
 echo "sh works/create.sh medovecot fuzz_test-mbox-from 1 only start targets"
+echo "gcc -c fuzz_test-target.c -o 11.o"
+echo "python3 works/oss-fuzz/parseCorpus.py fileName.tmp 124/ fuzz_name"
 echo "curl -F \"file=@test.txt\" https://file.io"
 echo "7z x /tmp/in/1.7z -o\"/tmp/in\" -p123321"
 echo "7z a -ssw -mx1 -p123321 -r0 /tmp/out/2.7z /tmp/out/"
