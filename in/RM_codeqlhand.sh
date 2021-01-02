70aa27df57ef2cf55caa1a32569f152b0ad428f7
ihsinme <61293369+ihsinme@users.noreply.github.com>
2021-01-02 17:05:18 +0300

    Update RM_codeqlhand.sh

--- in/RM_codeqlhand.sh
+++ in/RM_codeqlhand.sh
@@ -2,16 +2,15 @@
 
 echo "create projectFolder in works/"
 echo "export PATH=/opt/codeqlmy/codeql:$tmpPATH"
-echo "codeql resolve languages"
-echo "codeql resolve qlpacks"
-echo "codeql query format -i <file>"
+echo "codeql test run <qlref> --show-extractor-output"
+echo "codeql query format -i <ql>"
 echo "codeql generate query-help --format=markdown"
 echo "codeql database create cpp-database --language=cpp --command=make"
 echo "codeql query run file.ql -d=db1"
 echo "codeql test run file.qlref"
-echo "curl -F \"file=@test.txt\" https://file.io"
+echo "curl -F \"file=@/tmp/out/1.7z\" https://file.io"
 echo "7z x /tmp/1.7z -o\"/tmp/in\" -p123321"
-echo "7z a -ssw -mx1 -p123321 -r0 /tmp/2.7z /tmp/out/"
+echo "7z a -p123321 -r0 /tmp/out/1.7z /tmp/out/"
 while true; do
 sleep 3000
 done
