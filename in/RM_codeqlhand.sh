f45ce18f33f954de4a84235210bcc3fecf05fae7
ihsinme <61293369+ihsinme@users.noreply.github.com>
2021-01-11 23:26:02 +0300

    Update RM_codeqlhand.sh

--- in/RM_codeqlhand.sh
+++ in/RM_codeqlhand.sh
@@ -8,7 +8,7 @@ echo "codeql generate query-help --format=markdown"
 echo "codeql database create cpp-database --language=cpp --command=make"
 echo "codeql query run file.ql -d=db1"
 echo "curl -F \"file=@/tmp/out/1.7z\" https://file.io"
-echo "7z x /tmp/1.7z -o\"/tmp/in\" -p123321"
+echo "7z x /tmp/in/1.7z -o\"/tmp/in\" -p123321"
 echo "7z a -p123321 -r0 /tmp/out/1.7z /tmp/out/"
 while true; do
 sleep 3000
