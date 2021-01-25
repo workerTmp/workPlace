3b91c34a185aa4bdbf929acb2a444fa2ad3594ef
ihsinme <61293369+ihsinme@users.noreply.github.com>
2021-01-25 15:26:21 +0300

    Update tmux.sh

--- in/tmux.sh
+++ in/tmux.sh
@@ -7,3 +7,7 @@ tmux split-window -v
 tmux split-window -v 'sudo iftop'
 tmux split-window -h 'export NMON=c&&nmon'
 tmux split-window -h 'sudo /bin/sh /opt/RM.sh' 
+tmux select-pane -t 0
+tmux split-window -h
+tmux select-pane -t 2
+tmux split-window -h
