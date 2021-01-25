91a5f506df37125453a3e5c1b6c69a43a4cdc65d
ihsinme <61293369+ihsinme@users.noreply.github.com>
2021-01-25 14:42:23 +0300

    Update RM.sh

--- in/RM.sh
+++ in/RM.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "export PROMPT_COMMAND=\"history -a;\""
 echo "curl -F \"file=@test.txt\" https://file.io"
 echo "7z x /tmp/1.7z -o\"/tmp/in\" -p123321"
 echo "7z a -ssw -mx1 -p123321 -r0 /tmp/2.7z /tmp/out/"
