9986686f92558b0be954f86118ad4d9e8648f017
ihsinme <ihsinme@gmail.com>
2021-03-04 16:11:16 +0300

    Update RM_mLGTM.sh

--- in/RM_mLGTM.sh
+++ in/RM_mLGTM.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "export PROMPT_COMMAND=\"history -a;\""
-echo "7z x -/tmp/in/1.7z -o\"/opt/tmpFolderIssueCodeql/Develops\" -pchange123321"
+echo "sudo 7z a -r0 /tmp/out/1.7z /opt/tmpFolderIssueCodeql/Develops -pchange123321new"
 echo "curl -F \"file=@/tmp/out/1.7z\" https://file.io"
 echo "7z x /tmp/in/1.7z -o\"/tmp/in\" -p123321"
 echo "7z a -p123321 -r0 /tmp/out/1.7z /tmp/out/"
